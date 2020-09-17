c9fa12afa239b4f7312f7b49b44ede88d4120410
ya95123 <ya95123@gmail.com>
2020-09-17 15:58:54 +0800

    layout

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,23 @@
 <template>
   <v-app id="app">
-
+    <v-card>
+      <v-card-title>
+        Nutrition
+        <v-spacer></v-spacer>
+        <v-text-field
+          v-model="search"
+          append-icon="mdi-magnify"
+          label="Search"
+          single-line
+          hide-details
+        ></v-text-field>
+      </v-card-title>
+      <v-data-table
+        :headers="headers"
+        :items="desserts"
+        :search="search"
+      ></v-data-table>
+    </v-card>
   </v-app>
 </template>
 
@@ -8,9 +25,18 @@
 
 export default {
   name: 'App',
-
   data: () => ({
-    //
-  })
+    search: '',
+    headers: [
+      { text: '項目', align: 'center', value: 'number' },
+      { text: '地區', align: 'center', value: 'area' },
+      { text: '發布時間', align: 'center', value: 'time' },
+      { text: '最新消息', align: 'center', value: 'title' }
+    ],
+    desserts: []
+  }),
+  mounted () {
+
+  }
 }
 </script>
